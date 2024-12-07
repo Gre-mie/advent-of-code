9a41e63b64529b90b5b47c4ab80c253a41cc7492
Gre-mie <meow31-cat@outlook.com>
2024-12-07 13:38:54 +0000

    Check for valid equations using add_all() in 7-Dec/

--- 2024/7-Dec/main.py
+++ 2024/7-Dec/main.py
@@ -16,6 +16,26 @@ def orginise_data(file):
         dic[key] = tuple(map(lambda num: int(num), data[1].strip().split(' ')))
     return dic
 
+def add_all(arr):
+    total = 0
+    for num in arr:
+        total += num
+    return total
+
+def check_for(func, dic):
+    new_dic = dic.copy()
+    valid = []
+    for test_value, values in new_dic.items():
+        if func(values) == test_value:
+            valid.append(test_value)
+    return valid
+    
+def remove_valid(valid_list, dic):
+    new_dic = dic.copy()
+    for key in valid_list:
+        del new_dic[key]
+    return new_dic
+
 
 
 def main():
@@ -23,9 +43,27 @@ def main():
     puzzle_file = orginise_data(get_file("./puzzle-input.txt"))
     test_file = orginise_data(get_file("./test-input.txt"))
 
-    print(test_file, "\n")
-
     advent_intro(2024, 7)
+    equations = puzzle_file.copy()
+    valid_equations = []
+
+#    print(f"\nequations len:      ", len(equations))
+ #   print(f"valid equations len:", len(valid_equations))
+  #  print()
+
+    valid_equations = check_for(add_all, equations)
+    equations = remove_valid(valid_equations, equations) 
+
+    
+
+    print(f"\nequations len:      ", len(equations))
+    print(f"valid equations len:", len(valid_equations))
+    print()
+    
+    print(valid_equations)
+            
+    answer(add_all(valid_equations))
+
 
 
 #---------------------------------------------
