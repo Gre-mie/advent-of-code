fd1dcd2eb98a0b54cb3a940fd704edab6846095e
Gre-mie <meow31-cat@outlook.com>
2024-12-07 12:35:08 +0000

    Split data in 7-Dec/

--- 2024/7-Dec/main.py
+++ 2024/7-Dec/main.py
@@ -4,33 +4,51 @@ def advent_intro(year, day, part=1):
 def answer(answer):
     print(f"ANSWER: {answer}\n")
 
+def get_file(path):
+    with open(path, 'r') as file:
+        return file.read().split('\n')
+
+def orginise_data(file):
+    dic = {}
+    for line in file:
+        data = line.split(':')
+        key = int(data[0])
+        dic[key] = tuple(map(lambda num: int(num), data[1].strip().split(' ')))
+    return dic
+
 
 
 def main():
     
-    
+    puzzle_file = orginise_data(get_file("./puzzle-input.txt"))
+    test_file = orginise_data(get_file("./test-input.txt"))
+
+    print(test_file, "\n")
 
     advent_intro(2024, 7)
 
-    dic = {
-        "key-one": (1,2,3),
-        "key-two": (4,5,6),
-        "key-three": (7,8,9),
-        "key-four": (10,11,12),
-        "key-five": (13,14,15)
-           }
-#    for key, values in dic:
- #       print(key)
-  #      print(values)
-    print("keys:", list(dic))
-    del dic["key-two"]
-    print("keys", list(dic))
 
-    items = dic.items()
-    for item in items:
-        print(item)
-    for key, values in items:
-        print(f"key: {key}  values: {values}")
+#---------------------------------------------
+
+#    dic = {
+ #       "key-one": (1,2,3),
+  #      "key-two": (4,5,6),
+   #     "key-three": (7,8,9),
+    #    "key-four": (10,11,12),
+     #   "key-five": (13,14,15)
+      #     }
+
+#    print("keys:", list(dic))
+ #   del dic["key-two"]
+  #  print("keys", list(dic))
+
+#    items = dic.items()
+ #   for item in items:
+  #      print(item)
+   # for key, values in items:
+    #    print(f"key: {key}  values: {values}")
+
+#-------------------------------------------------------
 
 
 if __name__ == "__main__":
