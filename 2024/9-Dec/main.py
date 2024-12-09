774b54d81fa34c87d8b65b02e2e8d661a4b15ae5
Gre-mie <meow31-cat@outlook.com>
2024-12-09 13:09:04 +0000

    Add: functions to expand disc map in 9-Dec/

--- 2024/9-Dec/main.py
+++ 2024/9-Dec/main.py
@@ -7,6 +7,29 @@ def answer(answer):
 def get_file(path):
     with open(path, 'r') as file:
         return list(map(lambda num: int(num) , file.read()))
+    
+def expand_element(element, index= -1):
+    exp_el = []
+    if index > -1:
+        for i in range(element):
+            exp_el.append(index)
+    else:
+        for i in range(element):
+            exp_el.append('.')
+    return exp_el
+
+def expand_disc_map(file_arr):
+    new_arr = []
+    file_index = 0
+    for i in range(len(file_arr)):
+        if i % 2 == 0:
+            new_arr += expand_element(file_arr[i], file_index)
+            file_index += 1
+        else:
+            new_arr += expand_element(file_arr[i])
+    return new_arr
+
+
 
 
 def main():
@@ -14,10 +37,14 @@ def main():
     puzzle_file = get_file("./puzzle-input.txt")
     test_file = get_file("./test-input.txt")
 
+    p_exp = expand_disc_map(get_file("./puzzle-input.txt"))
+    t_exp = expand_disc_map(get_file("./test-input.txt"))
+
 #--------------------------------------------------------
 
-    print(test_file)
-    print()
+    print(f"{test_file}\n\n{t_exp}\n")
+
+
 
 #--------------------------------------------------------
 
