7f3afc4b4b8dfe572c8f2f9dd9902768ab263f86
Gre-mie <meow31-cat@outlook.com>
2024-12-11 16:38:04 +0000

    Update: structure and add first condition in 11-Dec/

--- 2024/11-Dec/main.py
+++ 2024/11-Dec/main.py
@@ -7,20 +7,41 @@ def answer(answer):
 def get_file(path):
     with open(path, 'r') as file:
         return list(map(lambda num: int(num) , file.read().split(' ')))
+    
+def change_zero_els(arr):
+    new_arr = arr.copy()
+    for i in range(len(new_arr)):
+        if new_arr[i] == 0:
+            new_arr[i] = 1
+            print(f"element {new_arr[i]} changed to 1")
+    return new_arr
 
+def run(times_to_run, file):
+    stones_arr = file.copy()
+
+    for i in range(times_to_run):
+        stones_arr = change_zero_els(stones_arr)
+#--------------------------------------------
+
+#-------------------------------------------
+        print(f"after iteration {i+1}: {stones_arr}\n")
+    return stones_arr
 
 def main():
 
     puzzle_file = get_file("./puzzle-input.txt")
     test_file = get_file("./test-input.txt")
 
-    print(puzzle_file)
-    print(test_file)
+    #print(puzzle_file)
+    #print(test_file)
 
+    test = [0, 1, 10, 99, 999]
+    print(f"start: {test}\n")
 
+#-----------------------------------------------
+    print(f"final array:", run(2, test), '\n')
 
     advent_intro(2024, 11)
-
 
 
 
