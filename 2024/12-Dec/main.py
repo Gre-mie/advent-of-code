6c19d1bd2c52f4bb89e9868a3d8693d0c27a49d6
Gre-mie <meow31-cat@outlook.com>
2024-12-12 11:24:00 +0000

    Setup: in 12-Dec/

---
+++ 2024/12-Dec/main.py
@@ -0,0 +1,21 @@
+def advent_intro(year, day, part=1):
+    print(f"--- Advent of Code ---\n{day} December {year} - Part {part}\n")
+
+def answer(answer):
+    print(f"ANSWER: {answer}\n")
+
+def get_file(path):
+    with open(path, 'r') as file:
+        return file.read().split('\n')
+
+
+def main():
+    advent_intro(2024, 12)
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
