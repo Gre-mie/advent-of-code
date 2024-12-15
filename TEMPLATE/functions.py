fd0f8af0f1ae42ed739c23905d5b60f511b3a76a
Gre-mie <meow31-cat@outlook.com>
2024-12-15 06:50:22 +0000

    Add: TEMPLATE for new file structure

---
+++ TEMPLATE/functions.py
@@ -0,0 +1,9 @@
+def advent_intro(year, day, part=1):
+    print(f"--- Advent of Code ---\n{day} December {year} - Part {part}\n")
+
+def answer(answer):
+    print(f"ANSWER: {answer}\n")
+
+def get_file(path):
+    with open(path, 'r') as file:
+        return file.read().split('\n')
---
+++ TEMPLATE/main.py
@@ -0,0 +1,9 @@
+from functions import *
+
+def main():
+    print("\033[31m ----- ADD INTRO ----- \033[37m")
+
+
+
+if __name__ == "__main__":
+    main()
